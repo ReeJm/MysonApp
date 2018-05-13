554ba6d3210e9a48a3f959baab8cff127bd2c550
ReeJm <fjzxljm123@gmail.com>
2018-05-13 14:38:43 +0800

    php

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+
+ <head>
+	<title>MySonApp</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ </head>
+
+ <body>
+ 	<input type="text"><br/>
+ 	<input type="text"><br/>
+ 	<input type="button" value="登录"><br/>
+
+ 	html csss js -》 app + ajax <br/>
+
+    app <br/>
+
+    php <br/>
+
+    mysql <br/>
+
+ </body>
+
+
+</html>
+
+
